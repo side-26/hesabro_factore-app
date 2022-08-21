cfd2701d570736cb4f89c11bd4fdd7fae7c06592
side-26e <mr72302@gamil.com>
2022-08-21 11:29:53 +0430

    fix set new constant for project

---
+++ src/config/constant.variable.js
@@ -0,0 +1,31 @@
+export const constants = {
+  currency: 'ریال',
+  questions: {
+    postComentsQuestion: 'مایل به ثبت نظرات خود؟؟',
+    exit: 'می خواهید خارج شوید؟؟؟',
+    thankTxt: 'ممنون از مشارکت شما'
+  },
+  routes: {
+    auth: {
+      path: '/login'
+    },
+    factores: {
+      path: '/',
+      alias: '/factor'
+    },
+    factoresDetailes: {
+      path: '/factor_detailes'
+    },
+    question_naire: {
+      path: 'question_naire'
+    }
+  },
+  auth: {
+    lessOrMoreThanElCharectar: 'تلفن همراه باید ۱۱ عدد باشد',
+    emptyField: 'فیلد مورد نظر نمی تواند خالی باشد',
+    notANumber: 'شماره تلفن وارد شده عدد نمی باشد!!'
+  },
+  error404: {
+    title: 'اوپس چیزی اینجا نیست'
+  }
+}
