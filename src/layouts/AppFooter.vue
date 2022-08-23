3e187497eae80a15c25cefbc460b037bee8fc58d
side-26e <mr72302@gamil.com>
2022-08-23 16:11:30 +0430

    fix : build new footerLayout

---
+++ src/layouts/AppFooter.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <q-footer elevated class="bg-grey-6 text-white desktop-only">
+    <q-toolbar class="row justify-center">
+      <div class="text-center">
+        حقوق این وب سایت متعلق به گروه فناوری
+        <a href="https://avapardaz.vc" class="text-black avapardaz_link"
+          >آوا‌ پرداز</a
+        >
+        است
+      </div>
+    </q-toolbar>
+  </q-footer>
+</template>
+<script>
+export default {}
+</script>
+<style lang=""></style>
