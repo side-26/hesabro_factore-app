a468d1aff2601acb41f3b3bda0415fe7962fadb2
side-26e <mr72302@gamil.com>
2022-08-23 16:14:21 +0430

    style : set new styles for FactorPage

--- src/pages/FactorPage.vue
+++ src/pages/FactorPage.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <div class="">
+  <div class="table_container q-mx-auto">
     <BasicTable />
   </div>
 </template>
@@ -23,15 +23,31 @@ export default {
   }
 }
 </script>
-<template>
-  <div><BasicTable /></div>
-</template>
-<script>
-import BasicTable from 'components/Factors/BasicTable.vue'
-export default {
-  components: {
-    BasicTable
+<style lang="scss">
+@media screen and (max-width: 599.99px) {
+}
+@media screen and (min-width: 600px) {
+  .table_container {
+    width: 95%;
   }
 }
-</script>
-<style lang=""></style>
+@media screen and (max-width: 1023.99px) {
+}
+@media screen and (min-width: 1024px) {
+  .table_container {
+    width: 85%;
+  }
+}
+@media screen and (max-width: 1440px) {
+}
+@media screen and (min-width: 1440px) {
+  .table_container {
+    width: 80%;
+  }
+}
+@media screen and (min-width: 30000px) {
+  .table_container {
+    width: 60%;
+  }
+}
+</style>
